ec3801c2c689e5db347db46cd319734a373a2f6d
VaishnaviKumar98 <vaishnavi.kumar@craft.co>
2022-05-23 13:25:29 +0530

    IN-149| Uncommented credentials from providers
    Co-authored-by: dsurendran <surendran.duraisamy@craft.co>

--- terraform/deploy/env/staging/providers.tf
+++ terraform/deploy/env/staging/providers.tf
@@ -2,7 +2,7 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-  // credentials  = var.use_impersonation == false ? local.service_account_key : null
+  credentials  = var.use_impersonation == false ? local.service_account_key : null
   access_token = var.use_impersonation == false ? null : data.google_service_account_access_token.default[0].access_token
   // request_timeout = "60s"
 }
